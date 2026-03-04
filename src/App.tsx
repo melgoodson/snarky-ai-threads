849513a55b7ecb650421dabded70f2ee919587ed
Seludo, Jomel <qj-seludo@tip.edu.ph>
2026-03-05 03:06:47 +0800

    fix: scroll to top on every page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ScrollToTop } from "./components/ScrollToTop";
 import { CartProvider } from "./contexts/CartContext";
 import { useAnalytics } from "@/hooks/useAnalytics";
 import Index from "./pages/Index";
@@ -56,6 +57,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <ScrollToTop />
           <AppContent />
           <Routes>
             <Route path="/" element={<Index />} />
